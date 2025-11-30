1bb0dec3c47e6f4f779ef342178b59cbdfb245dd
DESKTOP-0S13J2P\PC <jblancom06@educantabria.es>
2025-11-30 18:01:51 +0100

    Ultimo commit de la practica, añadido un filtro en la page de lista a traves de botones

--- PracticaExamen/src/app/pages/products-list/products-list.ts
+++ PracticaExamen/src/app/pages/products-list/products-list.ts
@@ -1,22 +1,34 @@
-import { Component, inject } from '@angular/core';
+
 import { Iproduct } from '../../interfaces/iproduct';
 import { ProductsService } from '../../services/products-service';
 import { ProductCard } from "../../components/product-card/product-card";
+import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-products-list',
-  imports: [ProductCard],
+  imports: [ProductCard, FormsModule],
   templateUrl: './products-list.html',
   styleUrl: './products-list.css',
 })
 export class ProductsList {
   productsArray: Iproduct[];
   productsservice = inject(ProductsService);
+  filterCategory: string; //creo variable para guardar la categoria para filtrar
 
   constructor(){
     this.productsArray = [];
+    this.filterCategory = ''; //inicializo la categoria
   }
   ngOnInit(): void{
     this.productsArray = this.productsservice.getAllproducts();
   }
+
+  get productsFiltered():Iproduct[]{ //getter que devuelve los productos filtrados segun la categoria
+    if(!this.filterCategory){ //si no hay filtro seleccionado devuelve todos
+      return this.productsArray;
+    }
+    return this.productsArray.filter(p => p.category === this.filterCategory); // si hay filtro devuelve solo los de la categoria filtrada
+  }
+
 }
